9f02035a5af69f2f71b77757e533bd24f595d4f0
Gianluca Fiore <forod.g@gmail.com>
2016-06-23 18:34:32 +0200

    add doc.go for godoc's documentation

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+
+Package cdshelf provides a virtual album shelf generator
+cdshelf: a "Goodreads for music", similar to what already offered by Last.fm, from which it extrapolates the data, but more visual
+
+*/
+package cdshelf
